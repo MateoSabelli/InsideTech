5c0dc441a0866a874441971282d0809fc68d7538
Mateo Sabelli <mateosabelli4a@gmail.com>
2025-01-23 15:44:28 -0300

    fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { About } from "@/components/About";
 import { InfiniteGallery } from "@/components/Clients";
 import { Contact } from "@/components/Contact";
-import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-import { NavDemo } from "@/components/NavDemo";
 import { Services } from "@/components/Services";
 
 export default function Home() {
@@ -11,8 +9,8 @@ export default function Home() {
     
     <div className="min-h-screen   relative overflow-hidden ">
       {/* Blur effect */}
-      <div className="absolute top-0 left-0 w-[500px] h-[500px] bg-cyan-500/40 blur-[150px] rounded-full animate-blob -z-0" />
-       
+      <div className="absolute top-0 left-0 w-[500px] h-[500px] bg-cyan-500/40 blur-[150px] rounded-full animate-blob -z-0 hidden md:block" />
+        <div className="h-[10vh] sm:h-0"></div>
         <Hero />
         <InfiniteGallery />
         <Services />
