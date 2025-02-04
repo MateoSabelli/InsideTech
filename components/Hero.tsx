e29ff77a0df53c495ff4c942c2d7f29258a511c1
Mateo Sabelli <msabelli@uade.edu.ar>
2025-02-04 15:03:16 -0300

    fix imports

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import Link from "next/link";/* 
 import { ReactTyped } from "react-typed"; */
 import { FloatingIcons } from "./FloatingIcons";
-import { InfiniteGallery } from "./Clients";
 import BackgroundVideo from "./BackgroundVideo";
 
 export const Hero = () => {
